5124f4e48e0cb65a7cd495789ec0c7b3dfbddbc3
Daniru2007 <daniruayuka@gmail.com>
2022-12-14 19:55:46 +0530

    css for links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import classNames from "classnames";
 import { BsFillMoonFill } from "react-icons/bs";
 import { BsFillSunFill } from "react-icons/bs";
+
 import "./style/App.css";
+import "./style/library.css";
 
 function App() {
   // change theme
@@ -16,6 +18,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1 className="link">Hi there I'm a link</h1>
       <h1 className={`${theme}`}>Hello World!</h1>
       <button onClick={() => updateDarkTheme(!darkTheme)}>{themeIcon}</button>
     </div>
