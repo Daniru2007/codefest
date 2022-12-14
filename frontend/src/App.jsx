e17e02a6f4bc6ba648221ff156bfa3ae90896890
Daniru2007 <daniruayuka@gmail.com>
2022-12-14 19:16:42 +0530

    change theme functionality added :rocket:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
+import classNames from "classnames";
+import { BsFillMoonFill } from "react-icons/bs";
+import { BsFillSunFill } from "react-icons/bs";
 import "./style/App.css";
 
 function App() {
+  // change theme
+  const [darkTheme, updateDarkTheme] = useState(true);
+  const theme = classNames({ dark: darkTheme, light: !darkTheme });
+  var themeIcon = darkTheme ? (
+    <BsFillMoonFill style={{ fontSize: "25px", color: "e6bb00" }} />
+  ) : (
+    <BsFillSunFill style={{ fontSize: "25px", color: "yellow" }} />
+  );
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1 className={`${theme}`}>Hello World!</h1>
+      <button onClick={() => updateDarkTheme(!darkTheme)}>{themeIcon}</button>
     </div>
   );
 }
