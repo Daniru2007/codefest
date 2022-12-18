bb7e41209839b562a9eb2a1c3253b2529d98c21d
Daniru2007 <daniruayuka@gmail.com>
2022-12-18 22:07:06 +0530

    navbar added :rocket:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import classNames from "classnames";
 import { BsFillMoonFill } from "react-icons/bs";
 import { BsFillSunFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Tilty from "react-tilty";
 
 import ToggleButton from "./components/ToggleButton";
 import SVGBlob from "./components/SVGBlob";
@@ -20,10 +23,39 @@ function App() {
   );
 
   return (
-    <div className="App">
-      <SVGBlob />
-      <h1 className={`${theme}`}>Hello World!</h1>
-      <button onClick={() => updateDarkTheme(!darkTheme)}>{themeIcon}</button>
+    <div className={`App ${theme}`}>
+      <Router>
+        <nav className="navbar">
+          <div className="navbar__logo"></div>
+          <ul className="navbar__links">
+            <li className="navbar__links__item">
+              <Link to="/home">Home</Link>
+            </li>
+            <li className="navbar__links__item">
+              <Link to="/about-us">About us</Link>
+            </li>
+            <li className="navbar__links__item">
+              <Link to="/study">Study</Link>
+            </li>
+            <li className="navbar__links__item">
+              <Link to="/time">Time</Link>
+            </li>
+          </ul>
+          <div className="navbar__right">
+            <label className="switch">
+              <input
+                type={"checkbox"}
+                defaultChecked={darkTheme}
+                onChange={() => updateDarkTheme(!darkTheme)}
+              ></input>
+              <span className="slider"></span>
+            </label>
+            <div className="navbar__right__user">
+              <FaUser fill="#1a1d22" fontSize="20px" />
+            </div>
+          </div>
+        </nav>
+      </Router>
     </div>
   );
 }
