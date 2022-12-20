5a2f90c4bddf9e8389ea454af8d83de94db4ecbc
ROCKYIO <97091503+NETHNU@users.noreply.github.com>
2022-12-20 22:26:32 +0530

    Add files via upload

---
+++ backend/register.php
@@ -0,0 +1,132 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+    session_start();
+    if (isset($_SESSION['SESSION_EMAIL'])) {
+        $email = $_POST["email"];
+        $_SESSION['otp'] = $otp;
+        $_SESSION['mail'] = $email;
+        $password = $_POST["password"];
+        header("Location: welcome.php");
+        die();
+    }
+    require 'vendor/autoload.php';
+
+    include 'config.php';
+    $msg = "";
+
+    if (isset($_POST['submit'])) {
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $password = mysqli_real_escape_string($conn, md5($_POST['password']));
+        $confirm_password = mysqli_real_escape_string($conn, md5($_POST['confirm-password']));
+        $code = mysqli_real_escape_string($conn, md5(rand()));
+        $otp = rand(1111,9999);
+
+        if (mysqli_num_rows(mysqli_query($conn, "SELECT * FROM users WHERE email='{$email}'")) > 0) {
+            $msg = "<div class='alert alert-danger'>{$email} - This email address has been already exists.</div>";
+        } else {
+            if ($password === $confirm_password) {
+                $sql = "INSERT INTO users (name, email, password, code) VALUES ('{$name}', '{$email}', '{$password}', '{$code}')";
+                $result = mysqli_query($conn, $sql);
+
+                if ($result) {
+                    echo "<div style='display: none;'>";
+                    $mail = new PHPMailer(true);
+
+                    try {
+                        //Server settings
+                        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+                        $mail->isSMTP();                                            //Send using SMTP
+                        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                        $mail->Username   = 'rockyio207nethnu@gmail.com';                     //SMTP username
+                        $mail->Password   = 'yqjiwzdmrmrcdrww';                               //SMTP password
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+                        //Recipients
+                        $mail->setFrom('rockyio207nethnu@gmail.com');
+                        $mail->addAddress($email);
+
+                        //Content
+                        $mail->isHTML(true);                                  //Set email format to HTML
+                        $mail->Subject = 'no reply';
+                        $mail->Body    = 'Here is the verification link <b><a href="http://localhost/3/?verification='.$code.'">http://localhost/3/?verification='.$code.'</a></b>';
+                        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                        $mail->send();
+                        echo 'Message has been sent';
+                    } catch (Exception $e) {
+                        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                    }
+                    echo "</div>";
+                    $msg = "<div class='alert alert-info'>We've send a verification link on your email address.</div>";
+                } else {
+                    $msg = "<div class='alert alert-danger'>Something wrong went.</div>";
+                }
+            } else {
+                $msg = "<div class='alert alert-danger'>Password and Confirm Password do not match</div>";
+            }
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>register</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta name="keywords"
+        content="Login Form" />
+    <link href="//fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css" type="text/css" media="all" />
+    <script src="https://kit.fontawesome.com/af562a2a63.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <section class="w3l-mockup-form">
+        <div class="container">
+            <div class="workinghny-form-grid">
+                <div class="main-mockup">
+                    <div class="">
+                        <span class=""></span>
+                    </div>
+                    <div class="w3l_form align-self">
+                        <div class="left_grid_info">
+                            <img src="2.png" alt="">
+                        </div>
+                    </div>
+                    <div class="content-wthree">
+                        <h2>Register Now</h2>
+                        <p>Register with us its only taking 2 - 3 min to register </p>
+                        <?php echo $msg; ?>
+                        <form action="" method="post">
+                            <input type="text" class="name" name="name" placeholder="Enter Your Name" value="<?php if (isset($_POST['submit'])) { echo $name; } ?>" required>
+                            <input type="email" class="email" name="email" placeholder="Enter Your Email" value="<?php if (isset($_POST['submit'])) { echo $email; } ?>" required>
+                            <input type="password" class="password" name="password" placeholder="Enter Your Password" required>
+                            <input type="password" class="confirm-password" name="confirm-password" placeholder="Enter Your Confirm Password" required>
+                            <button name="submit" class="btn" type="submit">Register</button>
+                        </form>
+                        <div class="social-icons">
+                            <p>Have an account! <a href="index.php">Login</a>.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="jquery.min.js"></script>
+    <script>
+        $(document).ready(function (c) {
+            $('.alert-close').on('click', function (c) {
+                $('.main-mockup').fadeOut('slow', function (c) {
+                    $('.main-mockup').remove();
+                });
+            });
+        });
+    </script>
+</body>
+</html>
