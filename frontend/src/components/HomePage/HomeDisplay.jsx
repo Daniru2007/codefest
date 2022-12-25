6f195af1d996a35db3a09839135dcea7dfe2baaf
Daniru2007 <daniruayuka@gmail.com>
2022-12-25 22:08:46 +0530

    let's goooooo! :yawning_face:

--- frontend/src/components/HomePage/HomeDisplay.jsx
+++ frontend/src/components/HomePage/HomeDisplay.jsx
@@ -3,15 +3,11 @@ import Tilty from "react-tilty";
 import "./HomeDisplay.css";
 
 function HomeDisplay({ setCursorOnLinks }) {
-  const cursor = {
-    onMouseEnter: () => setCursorOnLinks(true),
-    onMouseLeave: () => setCursorOnLinks(false),
-  };
   return (
     <div className="home-display">
       <h1>What do we Have?</h1>
       <div className="home-display__contents">
-        <Tilty className="home-display__content time" {...cursor}>
+        <Tilty className="home-display__content time">
           <h1>Time Management</h1>
           <p>
             We know that as a student, you have a lot on your plate - from
@@ -23,16 +19,15 @@ function HomeDisplay({ setCursorOnLinks }) {
             skills.{" "}
           </p>
         </Tilty>
-        <Tilty className="home-display__content study" {...cursor}>
+        <Tilty className="home-display__content study">
           <h1>Study</h1>
           <p>
-            We know that as a student, you have a lot on your plate - from
-            classes and assignments to extracurricular activities and personal
-            responsibilities. It can be overwhelming to try to balance
-            everything, but good time management skills can help you stay
-            organized and on top of your tasks. On our website, you will find a
-            variety of resources to help you improve your time management
-            skills.{" "}
+            We know that as a student, you are always looking for ways to
+            improve your study skills and get the most out of your education. On
+            our website, you will find a variety of resources to help you
+            succeed in your studies. From tips on how to create a study schedule
+            and set goals, to strategies for staying focused and retaining
+            information, we have something for every student.{" "}
           </p>
         </Tilty>
       </div>
