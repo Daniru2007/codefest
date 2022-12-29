3e7ed2f64780e49ecdda23f07d31b840d9eeda23
Daniru2007 <daniruayuka@gmail.com>
2022-12-29 19:19:06 +0530

    some updates on time project

--- frontend/src/components/Study/Study.jsx
+++ frontend/src/components/Study/Study.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import "./Study.css";
 
 function Study() {
--- frontend/src/components/TimeManagement/TimeProject.jsx
+++ frontend/src/components/TimeManagement/TimeProject.jsx
@@ -5,10 +5,24 @@ function TimeProject() {
   const [projectInfo, setProjectInfo] = useState({
     days: 0,
     time: [0, 0],
-    subjects: {
-      subject: 0,
-    },
+    subjects: [
+      { name: "science", val: 90 },
+      { name: "maths", val: 60 },
+      { name: "sinhala", val: 80 },
+      { name: "history", val: 40 },
+    ],
   });
+
+  const onSubjectChange = (e) => {
+    let parsedVal = parseInt(e.target.value ? e.target.value : "0");
+    let id = parseInt(e.target.id);
+    const subjects = projectInfo["subjects"];
+    subjects[id] = { name: subjects[id]["name"], val: parsedVal };
+    setProjectInfo({
+      ...projectInfo,
+      subjects: [...subjects],
+    });
+  };
 
   // this calls when some information is changed
   const onInfoChange = (e) => {
@@ -69,11 +83,26 @@ function TimeProject() {
         />
       </label>
       <br />
-      <label>
-        subject
-        <input type="range" min={0} max={100} />
-      </label>
-      <br />
+      {projectInfo["subjects"].map((subject) => {
+        const key = projectInfo.subjects.indexOf(subject);
+        return (
+          <div className="subject" key={key}>
+            <label>
+              {subject.name}
+              <input
+                type="range"
+                id={key}
+                value={subject.val}
+                min={0}
+                max={100}
+                onChange={onSubjectChange}
+              />
+              {subject.val}%
+            </label>
+            <br />
+          </div>
+        );
+      })}
       <button>Add</button>
     </div>
   );
