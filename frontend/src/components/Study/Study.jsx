14d33a1f14129571cbe8380264e796aba3e85c6d
falcon715 <muhammedfaahir2@gmail.com>
2023-01-05 17:42:38 +0530

    done

--- frontend/src/components/Study/Study.jsx
+++ frontend/src/components/Study/Study.jsx
@@ -7,10 +7,10 @@ function Study() {
   return (
     <div className="container">
       <h3>Start Your Learning Journey</h3>
-      <h1>Choose The Grade You Study</h1>
+      <h1>Choose The Grade You Study ?</h1>
       <div className="grades">
         <div className="grade">
-          <a href="#study">
+          <a href="#grade-6">
             <img src="assets/img_study/grade_6.png"></img>
           </a>
           <div className="grade6">
@@ -18,32 +18,15 @@ function Study() {
 
             <p>
               Education is not <br />
-              preparation for life; <br />
-              education is life itself. <br />
+              preparation for <br />
+              life education is <br />
+              life itself <br />
               —John Dewey.
             </p>
           </div>
         </div>
         <div className="grade">
-          <a href="#study">
-            <img src="assets/img_study/grade_8.png"></img>
-          </a>
-
-          <div className="grade8">
-            <h2>Grade 8</h2>
-
-            <p>
-              An investment in <br />
-              knowledge pays the <br />
-              best interest." <br />
-              —Benjamin Franklin.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="grades">
-        <div className="grade">
-          <a href="#study">
+          <a href="#grade-7">
             <img src="assets/img_study/grade_7.png"></img>
           </a>
 
@@ -58,9 +41,28 @@ function Study() {
             </p>
           </div>
         </div>
+      </div>
+
+      <div className="grades">
+        <div className="grade">
+          <a href="#grade-8">
+            <img src="assets/img_study/grade_8.png"></img>
+          </a>
+
+          <div className="grade8">
+            <h2>Grade 8</h2>
+
+            <p>
+              An investment in <br />
+              knowledge pays <br />
+              the best interest." <br />
+              -Benjamin Frankin.
+            </p>
+          </div>
+        </div>
 
         <div className="grade">
-          <a href="#study">
+          <a href="#grade-9">
             <img src="assets/img_study/grade_9.png"></img>
           </a>
 
