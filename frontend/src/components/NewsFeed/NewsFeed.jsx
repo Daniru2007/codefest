c03a36f359a83daa6bb50f297170af3676f3df79
Daniru2007 <daniruayuka@gmail.com>
2023-01-23 22:59:48 +0530

    nice news feed added

--- frontend/src/components/NewsFeed/NewsFeed.jsx
+++ frontend/src/components/NewsFeed/NewsFeed.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+
+import { Link, Outlet } from "react-router-dom";
+import { BsArrowLeftCircleFill } from "react-icons/bs";
 import "./NewsFeed.css";
 
 function NewsFeed(userId) {
@@ -8,21 +11,32 @@ function NewsFeed(userId) {
   const [fetched, changeFetch] = useState(false);
   useEffect(() => {
     fetch(
-      "https://newsapi.org/v2/everything?q=education&from=2022-12-23&sortBy=publishedAt&apiKey=aae5b1de3d1a4775b4f71d76940fa38d"
+      "https://newsapi.org/v2/everything?q=kids+science+below+18&from=2022-12-23&sortBy=publishedAt&apiKey=aae5b1de3d1a4775b4f71d76940fa38d"
     )
       .then((jsonObj) => {
         return jsonObj.json();
       })
       .then((tempData) => {
-        console.log(tempData);
         setData(tempData.articles);
       })
       .catch(() => changeFetch(!fetched));
   }, [fetched]);
   return (
-    <div>
+    <div className="news">
+      <Link to={"../"}>
+        <BsArrowLeftCircleFill className="back__button" />
+      </Link>
       {data?.map((news) => {
-        return <h1>{news.title}</h1>;
+        return (
+          <div className="news__item">
+            <h3>{news.title}</h3>
+            <img src={news.urlToImage} alt="" />
+            <a href={news.url} target="_blank">
+              {news.content}
+            </a>
+            <p>{news.author}</p>
+          </div>
+        );
       })}
     </div>
   );
